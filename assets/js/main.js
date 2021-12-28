e1a28b3b2797000538e21e16bc652ad553dc4324
frbaumgratz <frbaumgratz>
2021-12-28 12:16:59 -0300

    ajustando o slide do carrossel, as cards agora não deixam espaço em branco

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,11 +17,13 @@ function run() {
   changeCard();
 }
 
+// Eu alterei a function changeCard para que o carrossel travasse na última e na primeira card.
+// Assim, a tela fica sempre cheia de cards... Se você rola pra menos que 0, o index trava no 0. Se você rola pra mais que a última card, o index para na última.
 function changeCard() {
-  if (idx > card.length - 1) {
-    idx = 0;
+  if (idx > card.length - 4) {
+    idx = card.length - 4;
   } else if (idx < 0) {
-    idx = card.length - 1;
+    idx = 0;
   }
 
   cards.style.transform = `translateX(${-idx * 290}px)`;
